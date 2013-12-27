f9cf0698bd1ff7eed527cf7867dbffd93e12f5b4
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2013-12-27 07:35:59 +0100

    fixed paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,15 +13,48 @@ module.exports = function (grunt) {
                 }
             }
         },
-        clean: ["./build"]
+        clean: {
+            all: ["./build"],
+            app: ["./build/app"]
+        },
+        copy: {
+            app: {
+                expand: true,
+                cwd: 'src/',
+                src: '**',
+                dest: 'build/app/'
+            }
+
+        },
+        deploy: {
+            build: {
+                auth: {
+                    host: 'www.toobop.net',
+                    port: 21,
+                    authKey: 'site'
+                },
+                src: 'build',
+                dest: 'site'
+            }
+        },watch: {
+            app: {
+                files: ['src/**'],
+                tasks: ['default'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
     });
 
-    // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-dojo');
     grunt.loadNpmTasks('grunt-ftp-deploy');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Default task(s).
-    grunt.registerTask('default', ['clean', 'dojo:dist']);
+    grunt.registerTask('dist', ['clean:all', 'dojo']);
+    grunt.registerTask('default', ['copy:app']);
+    grunt.registerTask('deploy', ['deploy']);
 
 };
